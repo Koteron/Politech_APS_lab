5b78e7d59969216f0e76e5c5dab0924b61e0a5be
Koteron <viktor.nefedev.03@gmail.com>
2023-11-16 22:48:33 +0300

    Made literals into constants in Main

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,17 @@ import java.util.Scanner;
 
 public class Main
 {
+    private final static int BUFFER_SIZE = 5,
+            DISPATCH_INPUT_QUEUE_SIZE = 15,
+            DISPATCH_OUTPUT_QUEUE_SIZE = 1,
+            REQUEST_AMOUNT = 151;
     public static void main(String[] args)
     {
         ArrayList<Source> sources = new ArrayList<>();
         ArrayList<Device> devices = new ArrayList<>();
-
-        Buffer buffer = new Buffer(5);
-        DispatchInput dispatchInput = new DispatchInput(buffer,15);
-        DispatchOutput dispatchOutput = new DispatchOutput(devices, buffer, 1);
+        Buffer buffer = new Buffer(BUFFER_SIZE);
+        DispatchInput dispatchInput = new DispatchInput(buffer,DISPATCH_INPUT_QUEUE_SIZE);
+        DispatchOutput dispatchOutput = new DispatchOutput(devices, buffer, DISPATCH_OUTPUT_QUEUE_SIZE);
 
         sources.add(new Source(1, dispatchInput, 0.0));
         sources.add(new Source(2, dispatchInput, 0.0));
@@ -24,7 +27,7 @@ public class Main
         devices.add(new Device(6));
         devices.add(new Device(7));
 
-        Controller controller = new Controller(sources, 151, dispatchInput, buffer, dispatchOutput);
+        Controller controller = new Controller(sources, REQUEST_AMOUNT, dispatchInput, buffer, dispatchOutput);
         Scanner in = new Scanner(System.in);
         System.out.println("Enter 1 to start Step Mode and 2 to start Auto Mode: ");
         while (true)
